7d590a314b659c665643ff17dd2c6b0ac2e9104d
Siri <sirianil2004@gmail.com>
2026-03-16 22:26:45 +0530

    add cors middleware

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
+ import cors from 'cors';
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 import itemRoutes from './routes/itemRoutes';
